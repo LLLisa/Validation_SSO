c7b911d9151c2d7c050255e89919f44cf30801e4
Lisa Knox <lisaknox83@gmail.com>
2023-01-04 15:10:42 -0500

    nodemailer preliminary works

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Router } from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
 import morgan from 'morgan';
 import { userRouter } from './user';
@@ -15,6 +15,31 @@ app.use(express.static(path.join(__dirname, '../dist')));
 //router routes go here
 app.use('/user', userRouter);
 
+//nodemailer
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'justsingedthings@gmail.com',
+        pass: 'sunrnqlbwvgybtxf',
+    },
+});
+
+const mailConfig = {
+    from: 'justsingedthings@gmail.com',
+    to: 'lisaknox83@gmail.com',
+    subject: 'tets',
+    text: 'maybe works?',
+};
+
+transporter.sendMail(mailConfig, (err, info) => {
+    if (err) throw Error(err.message);
+    console.log('emaoil sent');
+    console.log(info);
+});
+
+//default route-------------------------------
 app.get('*', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
