74e58d535d9e5b82a68495f94bbfe126239c9673
Lisa Knox <lisaknox83@gmail.com>
2023-01-04 16:08:02 -0500

    twilio up, dotenv up

--- server/index.ts
+++ server/index.ts
@@ -12,32 +12,50 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../dist')));
 
-//router routes go here
+//router routes go here-------------------------
 app.use('/user', userRouter);
 
-//nodemailer
+//dotenv
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+//nodemailer------------------------------------
 import nodemailer from 'nodemailer';
+const { GMAIL_AUTH_USER, GMAIL_AUTH_PASS, GMAIL_FROM_ADDRESS } = process.env;
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'justsingedthings@gmail.com',
-        pass: 'sunrnqlbwvgybtxf',
+        user: GMAIL_AUTH_USER,
+        pass: GMAIL_AUTH_PASS,
     },
 });
 
 const mailConfig = {
-    from: 'justsingedthings@gmail.com',
+    from: GMAIL_FROM_ADDRESS,
     to: 'lisaknox83@gmail.com',
     subject: 'tets',
     text: 'maybe works?',
 };
 
-transporter.sendMail(mailConfig, (err, info) => {
-    if (err) throw Error(err.message);
-    console.log('emaoil sent');
-    console.log(info);
-});
+// transporter.sendMail(mailConfig, (err, info) => {
+//     if (err) throw Error(err.message);
+//     console.log('emaoil sent');
+//     console.log(info);
+// });
+
+//twilio-----------------------------------
+import twilio from 'twilio';
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUM } = process.env;
+const twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+// twilioClient.messages
+//     .create({
+//         body: 'testestestes',
+//         from: TWILIO_PHONE_NUM,
+//         to: '+16468585409',
+//     })
+//     .then((message) => console.log(message.sid));
 
 //default route-------------------------------
 app.get('*', (req: Request, res: Response) => {
